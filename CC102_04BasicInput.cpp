76eeda9cb7b121737650401d7fe019481c12cad2
Adriandebug-max <yapeadrian05@gmail.com>
2024-10-04 10:10:43 +0800

    Add files via upload

---
+++ CC102_04BasicInput.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	string Nickname, Course_Year, School;
+	
+	cout <<"Enter your Nickname\t:\t";
+	cin >> Nickname;
+	cout <<"Enter your Course-Year Level:\t";
+	cin >> Course_Year;
+	cout <<"Enter your School\t:\t";
+	cin >> School;
+	
+	cout <<" Wow Congrats "<< Nickname << " ! " << Course_Year << " is a nice cource.And you are studying in " << School << 
+	" which is one of the Center of Excellence in Teriary Education " <<endl;
+	
+	return 0;
+	
+}
+	

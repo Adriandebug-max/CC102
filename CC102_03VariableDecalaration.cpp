e454cb474669ad32d9bf0402dfa0e630dad527ad
Adriandebug-max <yapeadrian05@gmail.com>
2024-10-04 09:25:36 +0800

    Add files via upload

---
+++ CC102_03VariableDecalaration.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int age;
+	
+	cout <<"Enter your age\t:";
+	cin >> age;
+	cout <<"Your age is\t:" << age << endl;
+	cout <<"Thankyou"<<endl;
+	
+	return 0;
+	
+}

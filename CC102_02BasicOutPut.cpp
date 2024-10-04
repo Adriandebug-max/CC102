4b54cc0dc41047f719507a53bcecb5d50d518bc5
Adriandebug-max <yapeadrian05@gmail.com>
2024-10-04 09:11:09 +0800

    Add files via upload

---
+++ CC102_02BasicOutPut.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout <<"School\t\t\t:Our Lady Lourdes College Foundation"<<endl;
+	cout <<"Department\t\t:CCS Department"<<endl;
+	cout <<"Course & Year Level\t:BSCS 1A"<<endl;
+	cout <<"Your Full Name\t\t:Adrian H. Yape"<<endl;
+	cout <<"Address\t\t\t:P-1 Bagong Bayan, Jose Panganiban Camarines Norte"<<endl;
+	
+	return 0;
+	
+}
+	
